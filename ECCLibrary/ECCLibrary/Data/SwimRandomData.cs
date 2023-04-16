2bbe4208f73bc0f7f69f3c06726da64aad481ae0
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-04-16 00:35:11 -0400

    fix SwimRandomData docs

--- ECCLibrary/ECCLibrary/Data/SwimRandomData.cs
+++ ECCLibrary/ECCLibrary/Data/SwimRandomData.cs
@@ -10,19 +10,19 @@ public class SwimRandomData
     /// </summary>
     public float evaluatePriority;
     /// <summary>
-    /// The distance this creature wanders in each direction, every time the action is played.
+    /// The distance this creature can wander in any direction every time the action is played. X and Z values should always be identical and are usually 10 - 30 meters. Typically the vertical (Y) range is only 20% - 25% of the horizontal range.
     /// </summary>
     public Vector3 swimRadius;
     /// <summary>
-    /// Swim speed for this action (m/s).
+    /// Swim speed for this action (m/s). Typically 2-3 m/s for small fish, 4-8 m/s for medium fish and sharks, and 15-20 m/s for aggressive leviathans.
     /// </summary>
     public float swimVelocity;
     /// <summary>
-    /// The time in seconds between each change in direction.
+    /// The time in seconds between each change in direction. Typically 5 seconds, but more sporadic creatures may have values as low as 2 seconds.
     /// </summary>
     public float swimInterval;
     /// <summary>
-    /// If enabled, the creature will always swim to a point at the maximum distance from itself (as defined by <see cref="swimRadius"/>). Otherwise, the distance will be random.
+    /// In the vanilla game this value is typically set to 'true' for leviathans. If enabled, the creature will always swim towards a point at the maximum distance allowed by <see cref="swimRadius"/>. Otherwise, the distance will be random.
     /// </summary>
     public bool onSphere;
     /// <summary>
@@ -34,10 +34,10 @@ public class SwimRandomData
     /// Contains data pertaining to creating the <see cref="SwimRandom"/> CreatureAction.
     /// </summary>
     /// <param name="evaluatePriority">The priority for this <see cref="CreatureAction"/>, expected to be in the range [0, 1].</param>
-    /// <param name="swimRadius">The distance this creature wanders in each direction, every time the action is played.</param>
-    /// <param name="swimVelocity">Swim speed for this action (m/s).</param>
-    /// <param name="swimInterval">The time in seconds between each change in direction.</param>
-    /// <param name="onSphere">If enabled, the creature will always swim to a point at the maximum distance from itself (as defined by <see cref="swimRadius"/>). Otherwise, the distance will be random.</param>
+    /// <param name="swimRadius">The distance this creature can wander in any direction every time the action is played. X and Z values should always be identical and are usually 10 - 30 meters. Typically the vertical (Y) range is only 20% - 25% of the horizontal range.</param>
+    /// <param name="swimVelocity">Swim speed for this action (m/s). Typically 2-3 m/s for small fish, 4-8 m/s for medium fish and sharks, and 15-20 m/s for aggressive leviathans.</param>
+    /// <param name="swimInterval">The time in seconds between each change in direction. Typically 5 seconds, but more sporadic creatures may have values as low as 2 seconds.</param>
+    /// <param name="onSphere">In the vanilla game this value is typically set to 'true' for leviathans. If enabled, the creature will always swim towards a point at the maximum distance allowed by <see cref="swimRadius"/>. Otherwise, the distance will be random.</param>
     /// <param name="swimForward">The higher this value, the more this creature tends to continue swimming in the same direction. Default value is 0.5f. Typically higher (1.0f-1.2f) for larger creatures.</param>
     public SwimRandomData(float evaluatePriority, float swimVelocity, Vector3 swimRadius, float swimInterval = 5f, float swimForward = 0.5f, bool onSphere = false)
     {
