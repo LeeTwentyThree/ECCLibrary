4d49103f3cf64f39a258091fc04e564a8a87ba98
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-04-16 01:05:58 -0400

    fix SealedCreatureAsset docs

--- ECCLibrary/ECCLibrary/AssetClasses/SealedCreatureAsset.cs
+++ ECCLibrary/ECCLibrary/AssetClasses/SealedCreatureAsset.cs
@@ -8,17 +8,17 @@ namespace ECCLibrary;
 /// </summary>
 public sealed class SealedCreatureAsset : CreatureAsset
 {
-    private CreatureTemplate template;
-    private Func<GameObject, CreatureComponents, IEnumerator> modifyPrefab;
-    private Action<GameObject> applyMaterials;
+    private readonly CreatureTemplate template;
+    private readonly Func<GameObject, CreatureComponents, IEnumerator> modifyPrefab;
+    private readonly Action<GameObject> applyMaterials;
 
     /// <summary>
-    /// 
+    /// Creates a basic Creature Asset that does not use inheritance. Supply a template and any needed prefab code. Call the Register method to add the creature to the game.
     /// </summary>
-    /// <param name="prefabInfo"></param>
-    /// <param name="template"></param>
-    /// <param name="modifyPrefab"></param>
-    /// <param name="applyMaterials"></param>
+    /// <param name="prefabInfo">The unique PrefabInfo of this creature instance.</param>
+    /// <param name="template">The template that contains all of the creature's data.</param>
+    /// <param name="modifyPrefab">An optional call that allows for modification of the prefab after creation.</param>
+    /// <param name="applyMaterials">An optional call that, if overriden, no longer applies the MarmosetUBER shader automatically and allows for full control of the materials. Called AFTER <paramref name="modifyPrefab"/>!</param>
     public SealedCreatureAsset(PrefabInfo prefabInfo, CreatureTemplate template, Func<GameObject, CreatureComponents, IEnumerator> modifyPrefab = null, Action<GameObject> applyMaterials = null) : base(prefabInfo)
     {
         this.template = template;
@@ -27,7 +27,7 @@ public sealed class SealedCreatureAsset : CreatureAsset
     }
 
     /// <summary>
-    /// 
+    /// Handled by the class.
     /// </summary>
     /// <returns></returns>
     protected override CreatureTemplate CreateTemplate()
@@ -36,7 +36,7 @@ public sealed class SealedCreatureAsset : CreatureAsset
     }
 
     /// <summary>
-    /// 
+    /// Handled by the class.
     /// </summary>
     /// <param name="prefab"></param>
     /// <param name="components"></param>
@@ -48,7 +48,7 @@ public sealed class SealedCreatureAsset : CreatureAsset
     }
 
     /// <summary>
-    /// 
+    /// Handled by the class.
     /// </summary>
     /// <param name="prefab"></param>
     protected override void ApplyMaterials(GameObject prefab)
