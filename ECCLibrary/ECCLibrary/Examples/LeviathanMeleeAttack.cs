49f94c443c4b194fdc872a071ab170bcb61d6d9c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-04-17 20:54:50 -0400

    Create LeviathanMeleeAttack.cs

---
+++ ECCLibrary/ECCLibrary/Examples/LeviathanMeleeAttack.cs
@@ -0,0 +1,10 @@
+﻿namespace ECCLibrary.Examples;
+
+internal class LeviathanMeleeAttack : MeleeAttack
+{
+    public override void OnTouch(Collider collider)
+    {
+        base.OnTouch(collider);
+        ErrorMessage.AddMessage("Chomp!");
+    }
+}
