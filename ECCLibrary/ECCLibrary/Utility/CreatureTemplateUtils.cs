8c86b69e98e52e4d653e21fb43f162cb5ab19b46
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-05-26 20:19:17 -0400

    SetCreatureEssentials method

--- ECCLibrary/ECCLibrary/Utility/CreatureTemplateUtils.cs
+++ ECCLibrary/ECCLibrary/Utility/CreatureTemplateUtils.cs
@@ -3,14 +3,41 @@
 namespace ECCLibrary;
 
 /// <summary>
-/// Utilities for using the <see cref="CreatureTemplate"/> class more efficiently.
+/// Utility methods related to using the <see cref="CreatureTemplate"/> class more efficiently. An alternative to setting all properties manually.
 /// </summary>
 public static class CreatureTemplateUtils
 {
     /// <summary>
-    /// Sets properties related to generic prey creatures. Adds basic fear elements and allows it to be picked up and eaten.
+    /// <para>Sets the most commonly used properties for creatures.</para>
+    /// <para>Assigns the following properties:</para>
+    /// <list type="bullet">
+    /// <item><see cref="CreatureTemplate.CellLevel"/></item>
+    /// <item><see cref="CreatureTemplate.SwimRandomData"/></item>
+    /// <item><see cref="CreatureTemplate.RespawnData"/></item>
+    /// <item><see cref="CreatureTemplate.StayAtLeashData"/></item>
+    /// <item><see cref="CreatureTemplate.BioReactorCharge"/></item>
+    /// </list>
     /// </summary>
-    public static void SetupPreyEssentials(CreatureTemplate template, float fleeVelocity, PickupableFishData pickupable, EdibleData edible)
+    public static void SetCreatureEssentials(CreatureTemplate template, LargeWorldEntity.CellLevel cellLevel, SwimRandomData swimRandom, RespawnData respawnData, StayAtLeashData stayAtLeash = null, float bioReactorCharge = 200f)
+    {
+        template.CellLevel = cellLevel;
+        template.SwimRandomData = swimRandom;
+        template.RespawnData = respawnData;
+        template.StayAtLeashData = stayAtLeash;
+        template.BioReactorCharge = bioReactorCharge;
+    }
+
+    /// <summary>
+    /// <para>Sets properties related to generic prey creatures. Adds basic fear elements and allows it to be picked up and eaten.</para>
+    /// <para>Assigns the following properties so that you don't have to:</para>
+    /// <list type="bullet">
+    /// <item><see cref="CreatureTemplate.ScareableData"/></item>
+    /// <item><see cref="CreatureTemplate.FleeWhenScaredData"/></item>
+    /// <item><see cref="CreatureTemplate.PickupableFishData"/></item>
+    /// <item><see cref="CreatureTemplate.EdibleData"/></item>
+    /// </list>
+    /// </summary>
+    public static void SetPreyEssentials(CreatureTemplate template, float fleeVelocity, PickupableFishData pickupable, EdibleData edible)
     {
         template.ScareableData = new ScareableData();
         template.FleeWhenScaredData = new FleeWhenScaredData(0.8f, fleeVelocity);
