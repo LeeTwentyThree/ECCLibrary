09d55243c59978b22371ce6649d89705b637956b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-05-26 21:17:28 -0400

    CreatureTemplateUtils docs

--- ECCLibrary/ECCLibrary/Utility/CreatureTemplateUtils.cs
+++ ECCLibrary/ECCLibrary/Utility/CreatureTemplateUtils.cs
@@ -18,6 +18,13 @@ public static class CreatureTemplateUtils
     /// <item><see cref="CreatureTemplate.BioReactorCharge"/></item>
     /// </list>
     /// </summary>
+    /// <param name="template">The creature template to modify.</param>
+    /// <param name="cellLevel">Roughly determines how far this creature can be loaded in.</param>
+    /// <param name="mass">Mass in kg. Ranges from about 1.8f to 4050f. Default is 15kg.</param>
+    /// <param name="eyeFov">The FOV is used for detecting things such as prey.
+    /// SHOULD BE NEGATIVE! This value has an expected range of [-1, 0]. Is 0f by default. A value of -1 means a given object is ALWAYS in view.</param>
+    /// <param name="behaviourLod">Determines the distance for which certain calculations (such as Trail Managers) perform (or don't). It is recommended to increase these values for large creatures.</param>
+    /// <param name="bioReactorCharge">Total power output of this creature. All ECC creatures can be put in the bioreactor as long as this value is greater than 0.</param>
     public static void SetCreatureDataEssentials(CreatureTemplate template, LargeWorldEntity.CellLevel cellLevel, float mass, float eyeFov = 0f, BehaviourLODData behaviourLod = default, float bioReactorCharge = 200f)
     {
         template.CellLevel = cellLevel;
@@ -35,6 +42,9 @@ public static class CreatureTemplateUtils
     /// <item><see cref="CreatureTemplate.StayAtLeashData"/></item>
     /// </list>
     /// </summary>
+    /// <param name="template">The creature template to modify.</param>
+    /// <param name="swimRandom">Contains data pertaining to the <see cref="SwimRandom"/> action.</param>
+    /// <param name="stayAtLeash">Contains data pertaining to the <see cref="StayAtLeashPosition"/> action. This component keeps creatures from wandering too far.</param>
     public static void SetCreatureMotionEssentials(CreatureTemplate template, SwimRandomData swimRandom, StayAtLeashData stayAtLeash)
     {
         template.SwimRandomData = swimRandom;
@@ -51,6 +61,10 @@ public static class CreatureTemplateUtils
     /// <item><see cref="CreatureTemplate.EdibleData"/></item>
     /// </list>
     /// </summary>
+    /// <param name="template">The creature template to modify.</param>
+    /// <param name="fleeVelocity">The velocity in m/s at which the creature swims away from the player and other perceived threats.</param>
+    /// <param name="pickupable">Contains data pertaining to picking up and/or holding fish in your hands.</param>
+    /// <param name="edible">Contains data pertaining to the <see cref="Eatable"/> [sic] component.</param>
     public static void SetPreyEssentials(CreatureTemplate template, float fleeVelocity, PickupableFishData pickupable, EdibleData edible)
     {
         template.ScareableData = new ScareableData();
