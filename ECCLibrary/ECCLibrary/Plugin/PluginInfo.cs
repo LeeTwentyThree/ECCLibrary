a00f454cec74028b5ce11754f61bb82623d58790
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-05-26 21:52:24 -0400

    update name

--- ECCLibrary/ECCLibrary/Plugin/PluginInfo.cs
+++ ECCLibrary/ECCLibrary/Plugin/PluginInfo.cs
@@ -3,6 +3,6 @@
 internal class PluginInfo
 {
     public const string GUID = "com.lee23.ecclibrary";
-    public const string Name = "ECCLibrary";
+    public const string Name = "ECC Library";
     public const string Version = "2.0.0";
 }
