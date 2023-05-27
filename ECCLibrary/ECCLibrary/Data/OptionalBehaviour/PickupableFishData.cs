5e43aced60a9a2b74c0ebdcf6607e27ac997c3b7
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-05-26 21:16:17 -0400

    fix PickupableFishData constructors

--- ECCLibrary/ECCLibrary/Data/OptionalBehaviour/PickupableFishData.cs
+++ ECCLibrary/ECCLibrary/Data/OptionalBehaviour/PickupableFishData.cs
@@ -9,29 +9,36 @@ public class PickupableFishData
     /// Can this fish be held in the hotbar?
     /// </summary>
     public bool CanBeHeld;
+
     /// <summary>
     /// If can be held: The TechType that is used to find the holding animation.
     /// </summary>
     public TechType ReferenceHoldingAnimation;
+
     /// <summary>
     /// If can be held: The name of the model used for the World View, which must be a child of the object.
     /// </summary>
     public string WorldModelName;
+
     /// <summary>
     /// If can be held: The name of the model used for the First Person View, which must be a child of the object.
     /// </summary>
     public string ViewModelName;
 
     /// <summary>
-    /// Contains data pertaining to picking up and/or holding fish in your hands.
+    /// This constructor overload creates a <see cref="PickupableFishData"/> instance for creatures that can be picked up but NOT held.
     /// </summary>
-    /// <param name="canBeHeld">Can this fish be held in the hotbar?</param>
+    public PickupableFishData() { }
+
+    /// <summary>
+    /// This constructor overload creates a <see cref="PickupableFishData"/> instance for creatures that can be picked up AND held.
+    /// </summary>
     /// <param name="referenceHoldingAnimation">If can be held: The TechType that is used to find the holding animation.</param>
     /// <param name="worldModelName">If can be held: The name of the model used for the World View, which must be a child of the object.</param>
     /// <param name="viewModelName">If can be held: The name of the model used for the First Person View, which must be a child of the object.</param>
-    public PickupableFishData(bool canBeHeld, TechType referenceHoldingAnimation, string worldModelName, string viewModelName)
+    public PickupableFishData(TechType referenceHoldingAnimation, string worldModelName, string viewModelName)
     {
-        CanBeHeld = canBeHeld;
+        CanBeHeld = true;
         ReferenceHoldingAnimation = referenceHoldingAnimation;
         WorldModelName = worldModelName;
         ViewModelName = viewModelName;
