88cc1289cf68956441935301e32325ba7b627aeb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-11-14 15:42:16 -0500

    Update weird docs

--- ECCLibrary/ECCLibrary/Data/CreatureEssentials/RespawnData.cs
+++ ECCLibrary/ECCLibrary/Data/CreatureEssentials/RespawnData.cs
@@ -22,7 +22,7 @@ public class RespawnData
     /// Contains data pertaining to the <see cref="CreatureDeath"/> component.
     /// </summary>
     /// <param name="respawn">Whether the creature respawns or not.</param>
-    /// <param name="respawnOnlyIfKilledByCreature">If the creature can respawn at all, then this stops it from respawning when killed by the player. Should be FALSE for prey, and TRUE for predators!</param>
+    /// <param name="respawnOnlyIfKilledByCreature">If the creature can respawn at all, then this stops it from respawning when killed by the player. In the base game this is generally FALSE for passive fauna, and TRUE for aggressive fauna!</param>
     /// <param name="respawnInterval">How long it takes for this creature to respawn.</param>
     public RespawnData(bool respawn, bool respawnOnlyIfKilledByCreature = false, float respawnInterval = 300f)
     {
