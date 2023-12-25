530def248bb68c4519ce905f5d8c748bb6f17fff
Metious <71298690+Metious@users.noreply.github.com>
2023-12-25 12:53:39 +0330

    Added an example for a cloned creature

---
+++ ECCLibrary/ECCLibrary/Examples/ExampleClonedCreature.cs
@@ -0,0 +1,31 @@
+﻿using ECCLibrary.Data;
+
+namespace ECCLibrary.Examples;
+
+public class ExampleClonedCreature : CreatureAsset
+{
+    public ExampleClonedCreature(PrefabInfo prefabInfo) : base(prefabInfo)
+    {
+    }
+
+    protected override CreatureTemplate CreateTemplate()
+    {
+        var template = new CreatureTemplate(TechType.Boomerang, 420);
+        CreatureTemplateUtils.SetPreyEssentials(template, 4.20f, new PickupableFishData(TechType.Boomerang, null, null), new EdibleData(6f, 9f, false));
+
+        return template;
+    }
+
+    protected override IEnumerator ModifyPrefab(GameObject prefab, CreatureComponents components)
+    {
+        foreach (var renderer in prefab.GetComponentsInChildren<Renderer>())
+        {
+            foreach (var material in renderer.materials)
+            {
+                material.color = Color.green;
+            }
+        }
+        
+        yield break;
+    }
+}
