f0765302bcaf0241578db07f8b00f879d17af0d3
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-12-31 16:32:27 -0500

    Fix documentation problem

--- ECCLibrary/ECCLibrary/Assets/SealedCreatureAsset.cs
+++ ECCLibrary/ECCLibrary/Assets/SealedCreatureAsset.cs
@@ -13,7 +13,7 @@ public sealed class SealedCreatureAsset : CreatureAsset
     /// </summary>
     public Func<GameObject, CreatureComponents, IEnumerator> OnModifyPrefab { get; set; }
     /// <summary>
-    /// An optional call that, if overriden, no longer applies the MarmosetUBER shader automatically and allows for full control of the materials. Called AFTER <paramref name="modifyPrefab"/>!
+    /// An optional call that, if overriden, no longer applies the MarmosetUBER shader automatically and allows for full control of the materials. Called AFTER <paramref name="OnModifyPrefab"/>!
     /// </summary>
     public Action<GameObject> OnApplyMaterials { get; set; }
 
