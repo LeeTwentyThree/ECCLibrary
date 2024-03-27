8f7ae590c027310fe54ae720780325a4c0703853
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-03-26 20:21:59 -0400

    Clarification in CreatureTraitsData docs

--- ECCLibrary/ECCLibrary/Data/CreatureEssentials/CreatureTraitsData.cs
+++ ECCLibrary/ECCLibrary/Data/CreatureEssentials/CreatureTraitsData.cs
@@ -6,24 +6,24 @@
 public struct CreatureTraitsData
 {
     /// <summary>
-    /// The rate at which the creature gets hungrier. Predators often require higher levels of hunger to attack.
+    /// The rate at which the creature gets hungrier, per second. Predators often require higher levels of hunger to attack. A typical value for most predators is 0.01.
     /// </summary>
     public float HungerIncreaseRate;
     /// <summary>
-    /// The rate at which this creature becomes passive while actively hunting.
+    /// The rate at which this creature becomes passive while actively hunting, per second. A typical value for most predators is 0.05, around 0.1 if less aggressive.
     /// </summary>
     public float AggressionDecreaseRate;
     /// <summary>
-    /// The rate at which this creature becomes less scared. Used in very specific circumstances, most notably when taking damage.
+    /// The rate at which this creature becomes less scared, per second. Used in very specific circumstances, most notably when taking damage.
     /// </summary>
     public float ScaredDecreaseRate;
 
     /// <summary>
     /// Contains basic data pertaining to the <see cref="CreatureTrait"/>s of a creature.
     /// </summary>
-    /// <param name="hungerIncreaseRate">The rate at which the creature gets hungrier. Predators often require higher levels of hunger to attack.</param>
-    /// <param name="aggressionDecreaseRate">The rate at which this creature becomes passive while actively hunting.</param>
-    /// <param name="scaredDecreaseRate">The rate at which this creature becomes less scared. Used in very specific circumstances, most notably when taking damage.</param>
+    /// <param name="hungerIncreaseRate">The rate at which the creature gets hungrier, per second. Predators often require higher levels of hunger to attack. A typical value for most predators is 0.01.</param>
+    /// <param name="aggressionDecreaseRate">The rate at which this creature becomes passive while actively hunting, per second. A typical value for most predators is 0.05, around 0.1 if less aggressive.</param>
+    /// <param name="scaredDecreaseRate">The rate at which this creature becomes less scared, per second. Used in very specific circumstances, most notably when taking damage.</param>
     public CreatureTraitsData(float hungerIncreaseRate = 0.01f, float aggressionDecreaseRate = 0.05f, float scaredDecreaseRate = 0.1f)
     {
         HungerIncreaseRate = hungerIncreaseRate;
