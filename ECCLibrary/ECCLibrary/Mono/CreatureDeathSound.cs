6ede619741ad0bfd1ddaffa5899a743151d8a1c7
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-14 23:34:48 -0400

    Create CreatureDeathSound.cs

---
+++ ECCLibrary/ECCLibrary/Mono/CreatureDeathSound.cs
@@ -0,0 +1,34 @@
+﻿namespace ECCLibrary.Mono;
+
+/// <summary>
+/// This component automatically plays its associated death sound when the creature is killed.
+/// </summary>
+public class CreatureDeathSound : MonoBehaviour
+{
+    /// <summary>
+    /// The <see cref="FMOD_CustomEmitter"/> component that is played when the creature dies.
+    /// </summary>
+    public FMOD_CustomEmitter deathSoundEmitter;
+    
+    private bool _playedSound;
+
+    // This is a Unity event, called by the LiveMixin class
+    private void OnKill()
+    {
+        if (_playedSound)
+            return;
+        
+        _playedSound = true;
+        PlayDeathSound();
+    }
+
+    private void PlayDeathSound()
+    {
+        if (deathSoundEmitter == null)
+        {
+            ECCPlugin.logger.LogError("No emitter assigned to " + this);
+            return;
+        }
+        deathSoundEmitter.Play();
+    }
+}
